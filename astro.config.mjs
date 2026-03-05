0f11de1d1c3b2e238dc4655b80983b318f8e77f8
Robbie Veivers <robbieveivers@outlook.com>
2026-03-05 23:03:40 +1000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wavecoaching.github.io',
+  site: 'https://triwavecoaching.com',
   vite: {
     plugins: [tailwindcss()],
   },
